8a36ebf381013289df654894b752cc1c97cd2c54
jabelic <hibiki990523@gmail.com>
2021-02-04 22:11:20 +0900

    FIX: The domain of definition is symmetric to the origin.

--- euler.c
+++ euler.c
@@ -1,23 +1,31 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
-#define N 1000
+#define N 300
 #define EPS pow(10.0, -2.2)
 #define DT  0.01
-#define XLIM 50
+#define XLIM 60
 
-double f(double x){ return  x*x; }
-double df(double x){ return 2*x; }
-double euler(double *x){ 
-    x[0] = 0.001;
-    int times = 0;
-    int oldx = 0;
-    while (x[times] < XLIM && times < N){
-        times++;
-        x[times] = x[times-1] + df(x[times-1]) * DT;
-        printf("%f\n",x[times]);
+double f(double x){ return  x*x-2*x; }
+double df(double t){ return 3*t*t - 2; }
+double euler(double *t, double *x){ 
+    x[0] = 0.1;
+    int i = 0;
+    // int oldx = 0;
+
+    // while (x[i] < XLIM && i < N){
+    //     i++;
+    //     x[i] = x[i-1] + df(t[i]) * DT;
+    //     printf("%f, %f\n",x[i], x[i-1]);
+    // }
+    printf("%p\n", t);
+    
+    for (i = 1; i <= N; i++){
+        x[i] = x[i-1] + df(t[i]) * DT;
+        // printf("%f, %f\n",t[i], x[i]);
+        if (x[i] > XLIM || x[i] < -XLIM){ break; }
     }
-    return x[times];
+    return x[i];
 }
 
 int main(int argc, int **argv){
@@ -26,23 +34,29 @@ int main(int argc, int **argv){
     char name[32];
     double *x = calloc(N, sizeof(double));
     double *t = calloc(N, sizeof(double));
-    for(int i = 0; i <= N; i++){
-        t[i] = DT*i;
+    printf("%p\n", t);
+    int index = 0;
+    for(int i = -N/2; i <= N/2; i++){
+        t[index] = DT*i;
+        index++;
+        // printf("%f\n", t[i]);
     }
-    printf("%f\n", euler(x));
+    printf("%f\n", euler(t,x));
+    printf("%p\n", t);
+    printf("%f\n", t[1]);
     sprintf(name,"data.txt");
     f=fopen(name,"w");
     int plot_size = 0;
     fprintf(f,"%lf\t%lf\n",t[plot_size],x[plot_size]);
-    while (plot_size < N && x[plot_size] != 0.0){
+    while (plot_size < N){
         plot_size++;
         fprintf(f,"%lf\t%lf\n",t[plot_size],x[plot_size]);
     }
     fclose(f);
     gid=popen("gnuplot -persist","w");
     fprintf(gid,"set terminal png\n");
-    fprintf(gid,"set xrange [0:%f] \n", plot_size*DT); // fix me
-    fprintf(gid,"set yrange [-1.2:%f] \n", XLIM+0.1); // fix me
+    fprintf(gid,"set xrange [-%f:%f] \n", plot_size*DT,plot_size*DT); // fix me
+    fprintf(gid,"set yrange [-%f:%f] \n", (XLIM+0.1)/25,(XLIM+0.1)/25); // fix me
     fprintf(gid,"set output 'init.png' \n");
     fprintf(gid,"plot 'data.txt' w l \n");
     pclose(gid);
